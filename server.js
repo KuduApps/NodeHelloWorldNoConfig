fa15ab0196f8889663c5ddb8bc328b820c29f2eb
davidebbo <david.ebbo@microsoft.com>
2012-11-06 15:41:27 -0800

    Print url

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    console.log("Hello World");
+    console.log("Hello World " +req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello, world! [helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
 }).listen(process.env.PORT);  

4352a0c79406bc4d6f014f29eb170397b7ab4eaf
davidebbo <david.ebbo@microsoft.com>
2014-11-13 04:11:08 -0800

    Tweak output

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+    var name = 'world';
     console.log("Hello World " +req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello, world! [helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
+    res.end('<h2>Hello, ' + name + '!</h2> [iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
 }).listen(process.env.PORT);  
